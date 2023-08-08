954c71f820cd61539e3b1911cc334ce82f8ffcb5
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-08-08 09:31:41 +0200

    logout screen

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
 import "../scss/components_scss/Logout.scss";
-
+import { Link } from "react-router-dom";
+import { ReactComponent as Decoration } from "./../assets/Decoration.svg";
 
 export const Logout = () => {
   const auth = useAuth();
@@ -10,5 +11,25 @@ export const Logout = () => {
     auth.logout();
   }, [auth]);
 
-  return <div className="logout">ZROB LOGOUT</div>;
+  return (
+    <div className="signup-container">
+      <div className="signup-logging-section">
+        <div className="signup-header">
+          ZOSTAŁEŚ POPRAWNIE WYLOGOWANY
+          <div>
+            <Decoration />
+          </div>
+        </div>
+
+        <div className="signup-buttons">
+          <btn className="signup-btn">
+            <Link to="../login">Zaloguj się</Link>
+          </btn>
+          <btn className="signup-btn">
+            <Link to="../signup">Zarejestruj się</Link>
+          </btn>
+        </div>
+      </div>
+    </div>
+  );
 };
