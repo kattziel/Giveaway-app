90b9851681c8ea214a221bc3ab450712934c7cdb
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-08-08 14:10:03 +0200

    changing visible content according to active sections

--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -1,19 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Important from "./questionnaire/Important";
 import Steps from "./questionnaire/Steps";
 import QuestionnaireForm from "./questionnaire/QuestionnaireForm";
 import QuestionnaireControl from "./questionnaire/QuestionnaireControl";
 
 const Questionnaire = () => {
-  const [activeStep, setActiveStep] = useState(1);
+  const [activeSection, setActiveSection] = useState("giveaway-things");
+  // const [activeSection, setActiveSection] = useState("packing-instructions");
+  // const [activeSection, setActiveSection] = useState("localization");
+  // const [activeSection, setActiveSection] = useState("pickup");
+
+  const handleSectionChange = (newSection) => {
+    setActiveSection(newSection);
+  };
+
+  // useEffect(() => {
+  //   setActiveSection("giveaway-things");
+  // }, []);
+  // check whether you will be using it
 
   return (
     <>
-      <Important />
+      <Important activeSection={activeSection} />
       <div className="form-steps form-steps-flex">
-        <Steps />
-        <QuestionnaireForm />
-        <QuestionnaireControl/>
+        <Steps activeSection={activeSection} />
+        <QuestionnaireForm
+          activeSection={activeSection}
+          onSectionChange={handleSectionChange}
+        />
+        {/* <QuestionnaireControl activeSection={activeSection}/> */}
       </div>
     </>
   );
--- src/components/questionnaire/QuestionnaireForm.js
+++ src/components/questionnaire/QuestionnaireForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const QuestionnaireForm = ({ activeSection }) => {
+const QuestionnaireForm = ({ activeSection, onSectionChange }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState(null);
 
@@ -16,239 +16,220 @@ const QuestionnaireForm = ({ activeSection }) => {
   switch (activeSection) {
     case "giveaway-things":
       return (
-        <div className="questionnaire-content">
-          <div className="form-steps-header">Zaznacz, co chcesz oddać:</div>
-          <div className="form-steps-giveaway-options">
-            <div className="form-steps-giveaway-picker">
-              <label class="checkbox">
-                <input type="checkbox" />
-                <span></span>
-                ubrania, które nadają się do ponownego uzycia
-              </label>
-              <label class="checkbox">
-                <input type="checkbox" />
-                <span></span>
-                ubrania do wyrzucenia
-              </label>
-              <label class="checkbox">
-                <input type="checkbox" />
-                <span></span>
-                zabawki
-              </label>
-              <label class="checkbox">
-                <input type="checkbox" />
-                <span></span>
-                ksiązki
-              </label>
-              <label class="checkbox">
-                <input type="checkbox" />
-                <span></span>
-                inne
-              </label>
+        <>
+          <div className="questionnaire-content">
+            <div className="form-steps-header">Zaznacz, co chcesz oddać:</div>
+            <div className="form-steps-giveaway-options">
+              <div className="form-steps-giveaway-picker">
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  <span></span>
+                  ubrania, które nadają się do ponownego uzycia
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  <span></span>
+                  ubrania do wyrzucenia
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  <span></span>
+                  zabawki
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  <span></span>
+                  ksiązki
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  <span></span>
+                  inne
+                </label>
+              </div>
             </div>
           </div>
-        </div>
+          <div className="form-steps-buttons">
+            <button
+              className="next-btn"
+              onClick={() => onSectionChange("packing-instructions")}
+            >
+              Dalej
+            </button>
+          </div>
+        </>
       );
 
     case "packing-instructions":
       return (
-        <div className="questionnaire-content">
-          <div className="form-steps-header">
-            Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:
-          </div>
-          <div className="dropdown-section">
-            <div className="dropdown">
-              <div className="dropdown-menu">
-                <div className="dropdown-menu-title">
-                  <p>Liczba 60l worków:</p>
-                </div>
-                <div className="dropdown-menu-btn">
-                  <div className="dropdown-menu-text">
-                    <p>
-                      {selectedValue !== null ? selectedValue : "- wybierz -"}
-                    </p>
+        <>
+          <div className="questionnaire-content">
+            <div className="form-steps-header">
+              Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:
+            </div>
+            <div className="dropdown-section">
+              <div className="dropdown">
+                <div className="dropdown-menu" onClick={toggleDropdown}>
+                  <div className="dropdown-menu-title">
+                    <p>Liczba 60l worków:</p>
                   </div>
-                  <div className="dropdown-menu-icon">
-                    <img
-                      src={require("../../assets/Icon-Arrow-Up.png")}
-                      style={{
-                        width: "40px",
-                        height: "40px",
-                      }}
-                      alt="Arrow up"
-                    />
+                  <div className="dropdown-menu-btn">
+                    <div className="dropdown-menu-text">
+                      <p>
+                        {selectedValue !== null ? selectedValue : "- wybierz -"}
+                      </p>
+                    </div>
+                    <div className="dropdown-menu-icon">
+                      <img
+                        src={require("../../assets/Icon-Arrow-Up.png")}
+                        style={{
+                          width: "40px",
+                          height: "40px",
+                        }}
+                        alt="Arrow up"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="dropdown-menu-content">
-                <button onClick={() => selectValue("1")}>1</button>
-                <button onClick={() => selectValue("2")}>2</button>
-                <button onClick={() => selectValue("3")}>3</button>
-                <button onClick={() => selectValue("4")}>4</button>
-                <button onClick={() => selectValue("5")}>5</button>
+                <div className="dropdown-menu-content">
+                  <button onClick={() => selectValue("1")}>1</button>
+                  <button onClick={() => selectValue("2")}>2</button>
+                  <button onClick={() => selectValue("3")}>3</button>
+                  <button onClick={() => selectValue("4")}>4</button>
+                  <button onClick={() => selectValue("5")}>5</button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+          <div className="form-steps-buttons">
+            <button
+              className="next-btn"
+              onClick={() => onSectionChange("giveaway-things")}
+            >
+              Powrót
+            </button>
+            <button
+              className="next-btn"
+              onClick={() => onSectionChange("localization")}
+            >
+              Dalej
+            </button>
+          </div>
+        </>
       );
 
     case "localization":
-      return <div className="form-steps-number">Krok 3/4</div>;
-
-    case "pickup":
-      return <div className="form-steps-number">Krok 4/4</div>;
-
-    default:
       return (
-        <div className="questionnaire-content">
-          <div className="form-steps-header">
-            Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:
-          </div>
-          <div className="dropdown-section">
-            <div className="dropdown">
-              <div className="dropdown-menu">
-                <div className="dropdown-menu-title">
-                  <p>Liczba 60l worków:</p>
-                </div>
-                <div className="dropdown-menu-btn">
-                  <div className="dropdown-menu-text">
-                    <p>
-                      {selectedValue !== null ? selectedValue : "- wybierz -"}
-                    </p>
-                  </div>
-                  <div className="dropdown-menu-icon">
-                    <img
-                      src={require("../../assets/Icon-Arrow-Up.png")}
-                      style={{
-                        width: "40px",
-                        height: "40px",
-                      }}
-                      alt="Arrow up"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="dropdown-menu-content">
-                <button onClick={() => selectValue("1")}>1</button>
-                <button onClick={() => selectValue("2")}>2</button>
-                <button onClick={() => selectValue("3")}>3</button>
-                <button onClick={() => selectValue("4")}>4</button>
-                <button onClick={() => selectValue("5")}>5</button>
-              </div>
-            </div>
-          </div>
+        <div className="form-steps-buttons">
+          <button
+            className="next-btn"
+            onClick={() => onSectionChange("packing-instructions")}
+          >
+            Powrót
+          </button>
+          <button
+            className="next-btn"
+            onClick={() => onSectionChange("pickup")}
+          >
+            Dalej
+          </button>
         </div>
       );
 
-    // return (
-    //   <div className="questionnaire-content">
-    //       <div className="form-steps-header">Zaznacz, co chcesz oddać:</div>
-    //       <div className="form-steps-giveaway-options">
-    //         <div className="form-steps-giveaway-picker">
-    //           <label class="checkbox">
-    //             <input type="checkbox" />
-    //             <span></span>
-    //             ubrania, które nadają się do ponownego uzycia
-    //           </label>
-    //           <label class="checkbox">
-    //             <input type="checkbox" />
-    //             <span></span>
-    //             ubrania do wyrzucenia
-    //           </label>
-    //           <label class="checkbox">
-    //             <input type="checkbox" />
-    //             <span></span>
-    //             zabawki
-    //           </label>
-    //           <label class="checkbox">
-    //             <input type="checkbox" />
-    //             <span></span>
-    //             ksiązki
-    //           </label>
-    //           <label class="checkbox">
-    //             <input type="checkbox" />
-    //             <span></span>
-    //             inne
-    //           </label>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   );
+    case "pickup":
+      return (
+        <div className="form-steps-buttons">
+          <button
+            className="next-btn"
+            onClick={() => onSectionChange("localization")}
+          >
+            Powrót
+          </button>
+        </div>
+      );
+
+    default:
+      // return (
+      //   <div className="questionnaire-content">
+      //     <div className="form-steps-header">
+      //       Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:
+      //     </div>
+      //     <div className="dropdown-section">
+      //       <div className="dropdown">
+      //         <div className="dropdown-menu">
+      //           <div className="dropdown-menu-title">
+      //             <p>Liczba 60l worków:</p>
+      //           </div>
+      //           <div className="dropdown-menu-btn">
+      //             <div className="dropdown-menu-text">
+      //               <p>
+      //                 {selectedValue !== null ? selectedValue : "- wybierz -"}
+      //               </p>
+      //             </div>
+      //             <div className="dropdown-menu-icon">
+      //               <img
+      //                 src={require("../../assets/Icon-Arrow-Up.png")}
+      //                 style={{
+      //                   width: "40px",
+      //                   height: "40px",
+      //                 }}
+      //                 alt="Arrow up"
+      //               />
+      //             </div>
+      //           </div>
+      //         </div>
+      //         <div className="dropdown-menu-content">
+      //           <button onClick={() => selectValue("1")}>1</button>
+      //           <button onClick={() => selectValue("2")}>2</button>
+      //           <button onClick={() => selectValue("3")}>3</button>
+      //           <button onClick={() => selectValue("4")}>4</button>
+      //           <button onClick={() => selectValue("5")}>5</button>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      // );
+
+      return (
+        <>
+          <div className="questionnaire-content">
+            <div className="form-steps-header">Zaznacz, co chcesz oddać:</div>
+            <div className="form-steps-giveaway-options">
+              <div className="form-steps-giveaway-picker">
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  <span></span>
+                  ubrania, które nadają się do ponownego uzycia
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  <span></span>
+                  ubrania do wyrzucenia
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  <span></span>
+                  zabawki
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  <span></span>
+                  ksiązki
+                </label>
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  <span></span>
+                  inne
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="form-steps-buttons">
+            <button className="next-btn">Dalej</button>
+          </div>
+        </>
+      );
   }
 };
 
 export default QuestionnaireForm;
-
-// const QuestionnaireForm = () => {
-// return (
-//     <div>
-//       <div className="form-steps-header">Zaznacz, co chcesz oddać:</div>
-//       <div className="form-steps-giveaway-options">
-//         <div className="form-steps-giveaway-picker">
-//           <label class="checkbox">
-//             <input type="checkbox" />
-//             <span></span>
-//             ubrania, które nadają się do ponownego uzycia
-//           </label>
-//           <label class="checkbox">
-//             <input type="checkbox" />
-//             <span></span>
-//             ubrania do wyrzucenia
-//           </label>
-//           <label class="checkbox">
-//             <input type="checkbox" />
-//             <span></span>
-//             zabawki
-//           </label>
-//           <label class="checkbox">
-//             <input type="checkbox" />
-//             <span></span>
-//             ksiązki
-//           </label>
-//           <label class="checkbox">
-//             <input type="checkbox" />
-//             <span></span>
-//             inne
-//           </label>
-//         </div>
-//       </div>
-//     </div>
-// );
-// };
-
-// export default QuestionnaireForm;
-
-{
-  /* <div className="form-steps" - do usunięcia form steps>
-<div className="form-steps-number">Krok 2/4</div>
-
-<div className="form-steps-header">
-  Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:
-</div>
-
-<div className="dropdown-section">
-  <div className="dropdown">
-    <div className="dropdown-menu">
-      <div className="dropdown-menu-title">
-        <p>Liczba 60l worków:</p>
-      </div>
-      <div className="dropdown-menu-btn">
-        <div className="dropdown-menu-text">
-          <p>- wybierz - </p>
-        </div>
-        <div className="dropdown-menu-icon">
-          <img src={require("../../assets/Icon-Arrow-Up.png")} />
-        </div>
-      </div>
-    </div>
-    <div className="dropdown-menu-content">
-      <a href="#">1</a>
-      <a href="#">2</a>
-      <a href="#">3</a>
-      <a href="#">4</a>
-      <a href="#">5</a>
-    </div>
-  </div>
-</div>
-</div> */
-}
